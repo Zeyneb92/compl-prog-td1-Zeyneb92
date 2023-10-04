7c894a779b32eb3aef85846a6f7c0fc258dcdd75
Zeyneb Bouabdallaoui <zeynebo6@gmail.com>
2023-10-04 16:53:14 +0200

    Validation des changements

---
+++ Fraction.java
@@ -0,0 +1,37 @@
+
+public class Fraction {
+
+    private int num;
+    private int den;
+
+
+    public Fraction() {
+        this.num = 0;
+        this.den = 1;
+    }
+
+    public Fraction(int num, int den) {
+        this.num = num;
+        this.den = den;
+    }
+
+    public Fraction(int num) {
+        this.num = num;
+        this.den = 1;
+    }
+
+    public String toString() {
+        if (this.den==1 && )
+        return "Je suis une fraction.";
+    }
+
+    public int getNum() {
+		return num;
+	}
+
+	public int getDen() {
+		return den;
+	}
+ 
+
+}
---
+++ SimpleFraction/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Fraction f1 = new Fraction();
+        System.out.println(f1.toString());
+
+    
+        System.out.println();
+
+
+        Fraction f2 = new Fraction(2,4);
+        
+        
+
+    }
+}
